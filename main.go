ad61f213a82ebf7b04b1a1e8b5aa60330975cdb1
Andrey Inishev <inish777@gmail.com>
2017-06-27 22:37:44 +0700

    Dockerfile

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "net/http"
 
 func main() {
+	session, err := mgo.Dial("mongodb")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello world"))
 	})
